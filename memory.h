92262f525dba3f5ff1785e62ab00fc07f7b4d55c
Jack Farrelly <jf@studio-40.com>
2014-05-07 17:12:52 +0100

    memory headers

--- memory.h
+++ memory.h
@@ -1,3 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <time.h>
+#include <math.h>
+
 struct mem_info {
     long total;
     long free;
